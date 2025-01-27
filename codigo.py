eee7aafb51629f7b7b82c17a85298a9c677f0243
Your Name <you@example.com>
2025-01-27 04:38:29 -0300

    Algumas mudanças no codigo

--- codigo.py
+++ codigo.py
@@ -7,24 +7,28 @@ model = whisper.load_model("small")
 result = model.transcribe("/Users/Manacio/Documents/Ava07TASI/AVA07_TASI/alarmeMagnata.mp3") # endeço onde se localiza o audio que sera trancrito
 print(f"\n\nTexto Extraido do audio: {result['text']}")
 
+
+
 # SEGUNDA ETAPA: USAR O OLLAMA PARA FAZER UM RESUMO DA TRANCRIÇÃO DO AUDIO
 # o ollama sera utilizado para fazer um resumo do audio trancrito
 from langchain_ollama.llms import OllamaLLM
 model = OllamaLLM(model="llama3.2:latest") # Modelo que sera usado
-
 resposta = model.invoke("Faça um resumo o mais resumidamente possivel com os principais pontos-chaves do seguinte texto:  "+result['text']) # o texto extraido pelo o whisper sera resumido pelo Ollama
 print(f"\n\nResumo do Ollama: {resposta}")
 
-# TERCEIRA ETAPA: TORNARÁ O RESUMO DO ALLAMA EM ARQUIVO DE AUDIO
+
+
+# TERCEIRA ETAPA: TORNARÁ O RESUMO DO OLLAMA EM ARQUIVO DE AUDIO
 #!pip install git+https://github.com/suno-ai/bark.git
 from bark import SAMPLE_RATE, generate_audio, preload_models
-#  pip install ipython
+#  Instalação necessaria: pip install ipython
 from IPython.display import Audio
 
 preload_models()
 
+# Torna o resumo em um audio (processo talvez demorado, dependerá da capacidade de processamete de sua máquina)
 text_prompt = f"""
-      [PORTUGUESE-BR] {resposta}
+      [PORTUGUESE-BR] {resposta} 
  """
 audio_array = generate_audio(text_prompt)
 Audio(audio_array, rate=SAMPLE_RATE)
